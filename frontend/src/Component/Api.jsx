49e475502012693677e139596cd53070136d6bb5
vinita patidar <vinitapatidar2476>
2026-03-28 21:25:35 +0530

    updated API URL for deployment

--- frontend/src/Component/Api.jsx
+++ frontend/src/Component/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://job-portal-1-gl2w.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
