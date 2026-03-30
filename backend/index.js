1b6ef05afad177fb30b8674ae082f6f39041e554
vinita patidar <vinitapatidar2476>
2026-03-30 10:07:33 +0530

    final fix cors + routes

--- backend/index.js
+++ backend/index.js
@@ -8,27 +8,7 @@ const User = require("./models/User");
 const app = express();
 
 // Middleware
-// app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }));
-
-
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://job-portal-frontend-djt8.onrender.com"
-];
-
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(null, true); // 👈 IMPORTANT (temporary allow all)
-        }
-    },
-    credentials: true
-}));
-
-// 👇 VERY IMPORTANT (preflight fix)
-app.options("/*", cors());
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 app.use((req, res, next) => {
