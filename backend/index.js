8375c37b0ce70a01388dc60e30ef2206bff014af
vinita patidar <vinitapatidar2476>
2026-03-30 09:59:56 +0530

    fix cors duplicate error

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 // app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }));
-const cors = require("cors");
+
 
 const allowedOrigins = [
     "http://localhost:5173",
